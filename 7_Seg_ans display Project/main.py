23b391e5e69b69c7fd84a0ab18a19d8f5b5609b3
alphastor <ulliprajwal@gmail.com>
2021-03-30 22:03:02 +0530

    upgrading main_py code

--- 7_Seg_ans display Project/main.py
+++ 7_Seg_ans display Project/main.py
@@ -3,26 +3,32 @@ from display1 import *
 
 #---------------------------------------------------------------------------------------------------------------#
 
-funcs_1 = {'0': zero, '1': one, '2': two, '3': three, '4': four, '5': five, '6': six, '7': seven, '8': eight, '9': nine, '-': minus}  
-keys = ['0','1','2','3','4','5','6','7','8','9','-']
+funcs_1 = {'0': zero, '1': one, '2': two, '3': three, '4': four,
+           '5': five, '6': six, '7': seven, '8': eight, '9': nine, '-': minus}
+keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-']
 
-funcs_2 = {'0_2': zero_2, '1_2': one_2, '2_2': two_2, '3_2': three_2, '4_2': four_2, '5_2': five_2, '6_2': six_2, '7_2': seven_2, '8_2': eight_2, '9_2': nine_2}
+funcs_2 = {'0_2': zero_2, '1_2': one_2, '2_2': two_2, '3_2': three_2, '4_2': four_2,
+           '5_2': five_2, '6_2': six_2, '7_2': seven_2, '8_2': eight_2, '9_2': nine_2}
 keyz = ['0_2', '1_2', '2_2', '3_2', '4_2', '5_2', '6_2', '7_2', '8_2', '9_2']
 
 # creating tkinter window, win=TK() to win.mainloop() is the code for the elements of the window
-win = Tk() # body open
+win = Tk()  # body open
 win.title("7_Seg_Ans_Displayer")
 
 # st = usr_val.get()
+
+
 def calculate():
-    if "+" in usr_val.get():  
-        a = usr_val.get().partition("+") # separate the user val for ex: usr_input = 2+2 output = ('2', '+', '2')
-        b = int(a[0]) + int(a[2]) # convert the str to int and perform the calculation
+    if "+" in usr_val.get():
+        # separate the user val for ex: usr_input = 2+2 output = ('2', '+', '2')
+        a = usr_val.get().partition("+")
+        # convert the str to int and perform the calculation
+        b = int(a[0]) + int(a[2])
         print(b)
-        c = str(b) # converting b to str and store to c
+        c = str(b)  # converting b to str and store to c
         d = c[0]
 
-        if len(c)==2:
+        if len(c) == 2:
             if d == "-":
                 funcs_1[keys[10]]()
                 funcs_2[keyz[int(c[1])]]()
@@ -31,18 +37,21 @@ def calculate():
                 num_2 = int(c[1])
                 # print(num_1,num_2)
                 funcs_1[keys[num_1]]()
-                funcs_2[keyz[num_2]]() # for ex: if c[1] == "3" then c[1]+"_2" = "3_2" 
-        elif len(c)==1:
+                # for ex: if c[1] == "3" then c[1]+"_2" = "3_2"
+                funcs_2[keyz[num_2]]()
+        elif len(c) == 1:
             funcs_1[keys[0]]()
-            funcs_2[keyz[b]]() # find the desired number from the def functions 
-            
+            # find the desired number from the def functions
+            funcs_2[keyz[b]]()
+
     elif " - " in usr_val.get():
         a = usr_val.get().partition(" - ")
         b = int(a[0]) - int(a[2])
-        c = str(b) # converting b to str and store to c
+        print(b)
+        c = str(b)  # converting b to str and store to c
         d = c[0]
 
-        if len(c)==2:
+        if len(c) == 2:
             if d == "-":
                 funcs_1[keys[10]]()
                 funcs_2[keyz[int(c[1])]]()
@@ -51,18 +60,21 @@ def calculate():
                 num_2 = int(c[1])
                 # print(num_1,num_2)
                 funcs_1[keys[num_1]]()
-                funcs_2[keyz[num_2]]() # for ex: if c[1] == "3" then c[1]+"_2" = "3_2" 
-        elif len(c)==1:
+                # for ex: if c[1] == "3" then c[1]+"_2" = "3_2"
+                funcs_2[keyz[num_2]]()
+        elif len(c) == 1:
             funcs_1[keys[0]]()
-            funcs_2[keyz[b]]() # find the desired number from the def functions 
+            # find the desired number from the def functions
+            funcs_2[keyz[b]]()
 
     elif "*" in usr_val.get():
         a = usr_val.get().partition("*")
         b = int(a[0]) * int(a[2])
-        c = str(b) # converting b to str and store to c
+        print(b)
+        c = str(b)  # converting b to str and store to c
         d = c[0]
 
-        if len(c)==2:
+        if len(c) == 2:
             if d == "-":
                 funcs_1[keys[10]]()
                 funcs_2[keyz[int(c[1])]]()
@@ -71,18 +83,21 @@ def calculate():
                 num_2 = int(c[1])
                 # print(num_1,num_2)
                 funcs_1[keys[num_1]]()
-                funcs_2[keyz[num_2]]() # for ex: if c[1] == "3" then c[1]+"_2" = "3_2" 
-        elif len(c)==1:
+                # for ex: if c[1] == "3" then c[1]+"_2" = "3_2"
+                funcs_2[keyz[num_2]]()
+        elif len(c) == 1:
             funcs_1[keys[0]]()
-            funcs_2[keyz[b]]() # find the desired number from the def functions 
+            # find the desired number from the def functions
+            funcs_2[keyz[b]]()
 
     elif "/" in usr_val.get():
         a = usr_val.get().partition("/")
         b = int(a[0]) // int(a[2])
-        c = str(b) # converting b to str and store to c
+        print(b)
+        c = str(b)  # converting b to str and store to c
         d = c[0]
 
-        if len(c)==2:
+        if len(c) == 2:
             if d == "-":
                 funcs_1[keys[10]]()
                 funcs_2[keyz[int(c[1])]]()
@@ -91,32 +106,40 @@ def calculate():
                 num_2 = int(c[1])
                 # print(num_1,num_2)
                 funcs_1[keys[num_1]]()
-                funcs_2[keyz[num_2]]() # for ex: if c[1] == "3" then c[1]+"_2" = "3_2" 
-        elif len(c)==1:
+                # for ex: if c[1] == "3" then c[1]+"_2" = "3_2"
+                funcs_2[keyz[num_2]]()
+        elif len(c) == 1:
             funcs_1[keys[0]]()
-            funcs_2[keyz[b]]() # find the desired number from the def functions 
+            # find the desired number from the def functions
+            funcs_2[keyz[b]]()
 
     else:
         print("No")
 
+
 def clear():
-     length = len(usr_input.get())
-     usr_input.delete(length-20, 'end') #length-20 is number of characters to remove from inout box
+    # length = len(usr_input.get())
+    # length-20 is number of characters to remove from inout box
+    usr_input.delete(0, 'end')
 
 #---------------------------------------------------------------------------------------------------------------#
 
-btn_calculate =  Button(win, text = "Calculate", width = 10, command=calculate, font= "AnaEve") 
-btn_cleardisplay = Button(win, text = "Clear Display", width = 10, command=clear_display, font= "AnaEve")
-btn_clear = Button(win, text = "Clear", width = 10, command=clear, font= "AnaEve")
 
-usr_val = StringVar() # a varialbe to store user input as str type
-usr_input = Entry(win, textvariable = usr_val, font = 'Cooper 15') # initialising user input area
+btn_calculate = Button(win, text="Calculate", width=10,
+                       command=calculate, font="AnaEve")
+btn_cleardisplay = Button(win, text="Clear Display",
+                          width=10, command=clear_display, font="AnaEve")
+btn_clear = Button(win, text="Clear", width=10, command=clear, font="AnaEve")
 
-# mapping the button and input box on the window 
+usr_val = StringVar()  # a varialbe to store user input as str type
+# initialising user input area
+usr_input = Entry(win, textvariable=usr_val, font='Cooper 15')
+
+# mapping the button and input box on the window
 usr_input.grid(row=0, column=0)
 btn_clear.grid(row=0, column=1)
 btn_calculate.grid(row=1, column=0, sticky="ew")
 btn_cleardisplay.grid(row=1, column=1)
 
 #---------------------------------------------------------------------------------------------------------------#
-win.mainloop() # body close    
+win.mainloop()  # body close
--- 7_Seg_ans display Project/main_upgrade.py
+++ 7_Seg_ans display Project/main_upgrade.py
@@ -1,1 +1,27 @@
+#-----------import GUI builder module and a py file stored in same directory-----------#
 from tkinter import *
+# from display1 import *
+
+#-----------initialise window-----------#
+win = Tk()  # --provides a robust and platform independent windowing toolkit-- #
+win.title("Answer Display")  # --naming the window-- #
+
+win.geometry("300x500+900+150") # --widthxheight+posx+posy posx&y is where on the screen window should open-- #
+
+lbl = Label(win, text="---------✗alphastor✗---------", font='AnaEve')  # --display a text in window-- #
+lbl.place(x=6, y=5)
+
+screen = Entry(win, width=25, bd=1, font='10') # --initialise screen to display ans-- #
+screen.place(x=30, y=40)  # --positioning the screen-- #
+
+# --adding buttons to the window with a loop-- #
+buttons = []
+for i in range(10):
+    buttons.append(Button(width=7, text=str(i), bd=1, height=2))
+btn_txt = 1
+for i in range(0, 3):
+    for j in range(0, 3):
+        buttons[btn_txt].place(x=30+j*60, y=70+i*40)
+        btn_txt += 1
+
+win.mainloop()  # --run above codes in a loop till window is closed-- #
