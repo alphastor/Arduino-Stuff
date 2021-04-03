53498146e575e098b8c3a7e000d367fff9ebff76
alphastor <ulliprajwal@gmail.com>
2021-04-03 16:49:14 +0530

    .

--- Typing test Project/connectnano.py
+++ Typing test Project/connectnano.py
@@ -10,7 +10,17 @@ ser = Serial('COM6', baudrate=9600, timeout=1)
 def char_A():
     ser.write(b'a')
 
-def char_clear():
-    ser.write(b'-')
+def char_B():
+    ser.write(b'b')
 
-# def char_b():
+def char_C():
+    ser.write(b'c')
+
+def char_D():
+    ser.write(b'd')
+
+def char_E():
+    ser.write(b'e')
+
+def char_F():
+    ser.write(b'f')
--- Typing test Project/main_01.py
+++ Typing test Project/main_01.py
@@ -1,24 +1,78 @@
-from tkinter import *
+# from tkinter import *
 from connectnano import *
+from keyboard import *
+from time import sleep
 
 import os
 os.system('cls')
 
+func = {97:char_A, 98:char_B, 99:char_C, 100:char_D}
+# keyz = func.keys()
 
-func = {97:char_A}
-keyz = [97]
+while True:
+    if is_pressed('a'):
+        func.get(ord('a'))()
+    if is_pressed('b'):
+        func.get(ord('b'))()
+    if is_pressed('c'):
+        func.get(ord('c'))()
+    if is_pressed('d'):
+        func.get(ord('d'))()
+    # if is_pressed('e'):
+        # func.get(ord('e'))()
+    # if is_pressed('f'):
+        # func.get(ord('f'))()
+    # if is_pressed('g'):
+        # func.get(ord('g'))()
 
-def keys(event):
-    key = event.char
-    # print(key)
-    # print(ord(key))
-    func.get(ord(key))()
+# def keys(event):
+#     key = event.char
+#     func.get(ord(key))()
 
 #----------Window initialising----------#
-win = Tk()
-win.iconbitmap(r'D:\VS Code\P R O J E C T S\Arduino Stuff\Typing test Project\myicon.ico') #--r is to tell that its a path--#
-win.title('Typing Test Game')
-win.geometry('100x50+1250+100')
-win.bind("<Key>", keys)
+# win = Tk()
+# win.iconbitmap(r'D:\VS Code\P R O J E C T S\Arduino Stuff\Typing test Project\myicon.ico') #--r is to tell that its a path--#
+# win.title('Typing Test Game')
+# win.geometry('100x50+1250+100')
+# win.bind("<Key>", keys)
 
-win.mainloop()
+# win.mainloop()
+
+###################################################################################
+
+# from tkinter import *
+
+# running = True  # Global flag
+
+# def scanning():
+#     if running:  # Only do this if the Stop button has not been clicked
+#         print ("hello")
+
+#     # After 1 second, call scanning again (create a recursive loop)
+#     root.after(100, scanning)
+
+# def start():
+#     """Enable scanning by setting the global flag to True."""
+#     global running
+#     running = True
+
+# def stop():
+#     """Stop scanning by setting the global flag to False."""
+#     global running
+#     running = False
+
+# root = Tk()
+# root.title("Title")
+# root.geometry("500x500")
+
+# app = Frame(root)
+# app.grid()
+
+# start = Button(app, text="Start Scan", command=start)
+# stop = Button(app, text="Stop", command=stop)
+
+# start.grid()
+# stop.grid()
+
+# root.after(1000, scanning)  # After 1 second, call scanning
+# root.mainloop()
